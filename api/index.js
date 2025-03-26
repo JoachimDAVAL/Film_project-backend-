2443a666132cd662f8670cb4986d24458fb26500
JoachimDAVAL <joachim.daval01@gmail.com>
2025-03-26 11:24:49 +0100

    gyi

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import cors from "cors";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://film-project-frontend.vercel.app/',
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {

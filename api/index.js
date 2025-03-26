13bffdf93c3f0baf27d7e369a00195da35238f7c
JoachimDAVAL <joachim.daval01@gmail.com>
2025-03-26 11:38:56 +0100

    gy

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ import router from "./router.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extend: true }));
+
 app.use(cors({
   origin: 'https://film-project-frontend.vercel.app/',
 }));
-app.use(express.json());
-
-
 
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenue sur l'API !" });
@@ -19,6 +20,4 @@ app.get("/", (req, res) => {
 app.use('/api', router);
 
 // Exporter l'Express app comme une fonction Serverless
-export default (req, res) => {
-  app(req, res);
-};
+export default app;

83fb55e28b920799cfce031c879cd3658bbdb59f
JoachimDAVAL <joachim.daval01@gmail.com>
2025-03-26 11:40:37 +0100

    fgiu

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import router from "./router.js";
+
 
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extend: true }));
+
 
 app.use(cors({
   origin: 'https://film-project-frontend.vercel.app/',
